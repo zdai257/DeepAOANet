7132510b4eb7f32497f66117313a8dd4b1ffad2a
zdai-XPS_GTX1650 <zdai257@gmail.com>
2021-06-15 02:27:12 +0100

    A synthetic noisy R generator was created in ROS

---
+++ create_noisy.py
@@ -0,0 +1,68 @@
+import os
+from os.path import join
+import rospy
+from math import *
+import numpy as np
+from std_msgs.msg import String, Empty, Header, Float32, Float32MultiArray, MultiArrayDimension
+from pyargus import directionEstimation as de
+
+
+def callback(msg):
+    data = Float32MultiArray()
+
+    # Format 'data' to be publish
+    M0 = 4
+    msg_dimx = MultiArrayDimension()
+    msg_dimx.label = "height"
+    msg_dimx.size = M0
+    msg_dimx.stride = 2 * M0 * M0
+    data.layout.dim.append(msg_dimx)
+
+    msg_dimy = MultiArrayDimension()
+    msg_dimy.label = "width"
+    msg_dimy.size = M0
+    msg_dimy.stride = 2 * M0
+    data.layout.dim.append(msg_dimy)
+
+    msg_dimz = MultiArrayDimension()
+    msg_dimz.label = "imag"
+    msg_dimz.size = 2
+    msg_dimz.stride = 2
+    data.layout.dim.append(msg_dimz)
+
+    data.layout.data_offset = 0
+
+    # Parse received 'msg'
+    new_msg = Float32MultiArray()
+    new_msg.layout = msg.layout
+
+    M = msg.layout.dim[0].size
+    N = msg.layout.dim[1].size
+    len = M * N * 2
+
+    noise = np.random.normal(0, 0.001, len)
+    new_msg.data = msg.data + noise
+
+    new_samples = np.asarray(new_msg.data).reshape(M, N, 2)
+    data_real = new_samples[:, :, 0]
+    data_imag = new_samples[:, :, 1]
+    iq_samples = data_real + 1j*data_imag
+
+    # Get new R and publish
+    new_R = de.corr_matrix_estimate(iq_samples.T, imp="fast")
+    new_R_real = new_R.real
+    new_R_imag = new_R.imag
+
+    data_arr = np.append(new_R_real.reshape(M0, M0, 1), new_R_imag.reshape(M0, M0, 1), axis=2)
+    data_lst = list(data_arr.ravel())
+    data.data = data_lst
+    pub.publish(data)
+
+
+rospy.init_node('noisy_creator', anonymous=True)
+pub = rospy.Publisher('/kerberos/newR', Float32MultiArray, queue_size=10)
+
+topic = '/kerberos/iq_arr'
+rospy.Subscriber(topic, Float32MultiArray, callback)
+
+rospy.spin()
