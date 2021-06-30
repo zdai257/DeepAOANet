dece10d785c07cdc439a4a03469753a1e3810b20
zdai-XPS_GTX1650 <zdai257@gmail.com>
2021-07-01 00:15:06 +0100

    Created 5 replicas of Noisy LOS datasets; Stacked-LSTM perform best so far

---
+++ data_1606/Noi_R2csv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+END=5
+for ((i=2;i<=END;i++)); do
+    for bagfile in "noisy$i"/*.bag; do
+        echo $bagfile
+        csvname="${bagfile%.*}"
+        rostopic echo -b "$bagfile" -p /kerberos/R1e_4> "${csvname}_1e_4.csv"
+        rostopic echo -b "$bagfile" -p /kerberos/R5e_4> "${csvname}_5e_4.csv"
+        rostopic echo -b "$bagfile" -p /kerberos/R1e_5> "${csvname}_1e_5.csv"
+        rostopic echo -b "$bagfile" -p /kerberos/R5e_5> "${csvname}_5e_5.csv"
+    done
+done
--- data_1606/noisy/R2csv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for bagfile in *.bag; do
-    csvname="${bagfile%.*}"
-    rostopic echo -b "$bagfile" -p /kerberos/R1e_4> "${csvname}_1e_4.csv"
-    rostopic echo -b "$bagfile" -p /kerberos/R5e_4> "${csvname}_5e_4.csv"
-    rostopic echo -b "$bagfile" -p /kerberos/R1e_5> "${csvname}_1e_5.csv"
-    rostopic echo -b "$bagfile" -p /kerberos/R5e_5> "${csvname}_5e_5.csv"
-done
