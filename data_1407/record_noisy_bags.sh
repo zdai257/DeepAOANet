6111c21a61fc6760b5f737a59ff567c0cc9c711f
zdai-XPS_GTX1650 <zdai257@gmail.com>
2021-07-15 15:58:18 +0100

    Evaluate 'LOS+reflector' dataset with model trained by other three datasets: failed

---
+++ data_1407/record_noisy_bags.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+END=5
+for ((i=1;i<=END;i++)); do
+    for bagfile in *.bag; do
+        csvname="${bagfile%.*}"
+        rosbag record -O noisy$i/${csvname}.bag -x /kerberos/iq_arr __name:=my_bag &
+        sleep 2
+        rosbag play ${csvname}.bag
+        rosnode kill /my_bag
+    done
+done
