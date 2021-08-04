d92b9595bb0e913694bd3d31e0fd26085a4cc8a5
zdai-XPS_GTX1650 <zdai257@gmail.com>
2021-08-04 22:59:36 +0100

    Added testing datasets LOS and Corridor [-45, -25, 25, 45]

---
+++ testCorridor/R2csv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for bagfile in *.bag; do
+    csvname="${bagfile%.*}"
+    rostopic echo -b "$bagfile" -p /kerberos/r > "${csvname}.csv"
+    rostopic echo -b "$bagfile" -p /kerberos/doa_results > "music_${csvname}.csv"
+done
