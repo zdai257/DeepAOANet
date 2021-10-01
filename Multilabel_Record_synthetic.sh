3391b63a866be8c1f2552274d171eafe949a5b24
zdai-XPS_GTX1650 <zdai257@gmail.com>
2021-10-01 10:24:45 +0100

    Ready to generate 3*6 datasets

--- Multilabel_Record_synthetic.sh
+++ Multilabel_Record_synthetic.sh
@@ -11,8 +11,8 @@ thetaname1=$arg2
 thetaname2=$arg4
 dir1=data_0107
 dir2=data_0207
-END=31
-for ((i=31;i<=END;i++)); do
+END=32
+for ((i=32;i<=END;i++)); do
     cd /home/zdai/repos/GPSLoRaRX/${dir1}
     for bagfile1 in deg_*.bag; do
         csvname1="${bagfile1%.*}"
@@ -29,9 +29,9 @@ for ((i=31;i<=END;i++)); do
                 rosbag record -O ${degname1}_multi-${degname2}.bag -e "/kerberos/R_(.*)" __name:=my_bag &
                 sleep 2
                 cd /home/zdai/repos/GPSLoRaRX/${dir1}
-                rosbag play -r 0.5 ${csvname1}.bag /kerberos/iq_arr:=/IQ1 &
+                rosbag play -r 0.6 ${csvname1}.bag /kerberos/iq_arr:=/IQ1 &
                 cd /home/zdai/repos/GPSLoRaRX/${dir2}
-                rosbag play -r 0.6 ${csvname2}.bag /kerberos/iq_arr:=/IQ2
+                rosbag play -r 0.8 ${csvname2}.bag /kerberos/iq_arr:=/IQ2
                 sleep 5
                 rosnode kill -a
             fi
